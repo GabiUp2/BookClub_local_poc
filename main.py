17a5321100ac8bf86ca7571cfdde2ec8c7022111
BartoszWichowski <bartosz.wichowski@dxc.com>
2026-01-10 10:00:00 +0100

    refactor: improve logging configuration formatting and update Prometheus job name
    - Reformatted the logging configuration in main.py for better readability.
    - Updated the job name in prometheus.yml from 'bookclub-server' to 'bookclub-preprocessing-server' to reflect the new server context.

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
-logger.addHandler(logging.FileHandler("./logs/main.log", mode="a", encoding="utf-8", delay=False))
+logger.addHandler(
+    logging.FileHandler("./logs/main.log", mode="a", encoding="utf-8", delay=False)
+)
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(lineno)d - %(funcName)s - %(process)d - %(thread)d - %(threadName)s - %(message)s')
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(lineno)d - %(funcName)s - %(process)d - %(thread)d - %(threadName)s - %(message)s"
+)
 logger.handlers[0].setFormatter(formatter)
 logger.handlers[1].setFormatter(formatter)
 
